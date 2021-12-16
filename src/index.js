b0a7aea840005f378f33b08eb2ad3a35c06c021e
Tatiana Muniz <tati.muniz0@gmail.com>
2021-12-16 20:27:24 +0300

    iniciando projeto

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+const { Client, TextContent, WebhookController } = require('@zenvia/sdk');
+
+dotenv.config();
+
+const client = new Client(process.env.ZENVIA_TOKEN);
+
+const whatsapp = client.getChannel('whatsapp');
+
+const webhook = new WebhookController({
+    messageEventHandler: (messageEvent) => {
+        console.log('Message event:', messageEvent);
+
+        const content = new TextContent('Testado');
+
+        whatsapp.sendMessage(messageEvent.message.to, messageEvent.message.from, content)
+            .then(response => {
+                console.log('Response:', response);
+            });
+    },
+    channel: 'whatsapp',
+});
+
+webhook.on('listening', () => {
+    console.log('Webhook is listening');
+});
+
+webhook.init();
