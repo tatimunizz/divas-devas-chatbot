8316d4c016933d3d635637b292540a5e095a0f47
Tatiana Muniz <tati.muniz0@gmail.com>
2022-01-19 13:05:53 +0300

    Inclundo docker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const { Client } = require('@zenvia/sdk');
 const { webhookController } = require('./controllers/webhookController');
 
 dotenv.config();
-
 const client = new Client(process.env.ZENVIA_TOKEN);
 const whatsapp = client.getChannel("whatsapp");
 const webhook = webhookController(whatsapp, process.env.AUDD_TOKEN);
