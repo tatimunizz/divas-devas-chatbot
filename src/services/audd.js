f394171f7a0bd019b2beebab3465710b4dc93035
Tati Muniz <tati.muniz0@gmail.com>
2022-01-24 18:55:22 +0300

    Update audd.js

--- src/services/audd.js
+++ src/services/audd.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 
 const request = async (url, token) => {
   const response = await axios.post({
-    //method: "post",
     url: "https://api.audd.io/",
     data: {
       api_token: token,
